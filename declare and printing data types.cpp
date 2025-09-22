2e36d7f7829c90199d0fe5838627a93bfb362c49
25bcs022 <25bcs022@kgcas.com>
2025-09-22 12:01:27 +0530

    first commit

---
+++ declare and printing data types.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a=12;
+	float b=15.123;
+	char c='A';
+	double d=15.123455;
+	printf("the given number of int datatype is %d\n",a);
+	printf("the given number of float datatype is %f\n",b);
+	printf("the given number of char datatype is %c\n",c);
+	printf("the given number of double datatype is %f\n",d);
+	return 0;
+}
---
+++ finding size of different data types.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	printf("size of char: %1u byte\n", sizeof(char));
+	printf("size of int: %1u byte\n", sizeof(int));
+	printf("size of long: %1u byte\n", sizeof(long));
+	printf("size of float: %1u byte\n", sizeof(float));
+	printf("size of double: %1u byte\n", sizeof(double));
+	return 0;
+}
